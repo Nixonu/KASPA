8f142acc3ed1f6723a747dc92419b84cf52faf90
Nixonu <nixonu@ya.ru>
2021-12-17 22:02:48 +0500

    Update kaspamining.sh

--- kaspamining.sh
+++ kaspamining.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+miner stop
 /home/user/kaspa-miner-v0.1.3-GPU-with-workload-linux-amd64 --kaspad-address 176.9.24.84 --mining-address kaspa:qqs5sr5art24q94kcwearss0kpt6c2j6gncusza83lvw6w783haajl3k7xak4 --gpu-threads 10 --threads 10
